22e64c32a4330ec2f6a7b785d242aa720292f9d6
Alex Geis <siegxela@gmail.com>
2022-08-28 06:42:50 -0600

    init commit - adding createShip factory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import "./css/style.scss";
 
-// inital test
-console.log("hello world!");
+// length = 2-5
+function createShip(length) {
+	const hit = (number) => {
+		// mark the provided position number as a hit
+		console.log("hit", number);
+	};
+
+	const isSunk = () => {
+		// return true/false based on ship length and whether all of ship positions are ‘hit’
+		console.log("isSunk");
+	};
+
+	return {
+		length,
+		hit,
+		isSunk,
+	};
+}
+const test = createShip(2);
+console.log(test);
