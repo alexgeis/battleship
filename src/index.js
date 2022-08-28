55e9d1ba797077c01a96dc9036e9d296cda1eea1
Alex Geis <98363461+alexgeis@users.noreply.github.com>
2022-08-28 06:22:03 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./css/style.scss";
+
+// inital test
+console.log("hello world!");
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		index: "./src/index.js",
+		// print: "./src/otherFunction.js",s
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Todo List",
+			template: "src/index.html",
+		}),
+		new MiniCssExtractPlugin(),
+		// access to environment variables
+		new Dotenv({ systemvars: true }),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(s(a|c)ss)$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	resolve: {
+		extensions: ["*", ".js", ".jsx"],
+		fallback: {
+			fs: false,
+			child_process: false,
+		},
+	},
+	optimization: {
+		runtimeChunk: "single",
+	},
+};
